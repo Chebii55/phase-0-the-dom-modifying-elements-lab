539e69c16ece1901ee7e32deb42e4c0118d7059c
Sheila Chebii <shillerchier055@gmail.com>
2023-10-17 13:27:03 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const neww=document.querySelector('main');
+    neww.remove();
+const newHeader=document.createElement("h1");
+newHeader.id='victory'
+newHeader.textContent=('Sheila is the champion')
+newHeader.append();
+    
+   
